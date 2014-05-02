214913c3d092bb4f17d405ed48ef255758f83117
Christian Kyony <ckyony@changamuka.com>
2014-05-02 15:51:27 +0200

    8.2.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe User do
 
-  before :each do
+  before do
     @user = User.new(name: "Example User", email: "user@example.com",
-                    password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
+
   subject {@user}
 
   it { should respond_to(:name)}
@@ -13,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate)}
   it { should be_valid }
 
@@ -78,13 +80,19 @@ describe "return value of authenticate method" do
     it { should eq found_user.authenticate(@user.password) }
   end
 
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
   end
-end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 
 
 
