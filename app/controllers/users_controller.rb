492830211de4442598afc4f9638f087a79e0857a
Christian Kyony <ckyony@changamuka.com>
2014-05-03 01:34:49 +0200

    9.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @user = User.new
   end
-
-
 
   def show
     @user = User.find(params[:id])
