c06487be40624a4afa80f47204a537513faf2c18
Christian Kyony <ckyony@changamuka.com>
2014-05-02 16:54:25 +0200

    finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,5 +20,14 @@ module SessionsHelper
     ! current_user.nil?
   end
 
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
+
+
 
 end
