571eea2b01a28c59617ecb7417f5f8b7bbf5763b
Christian Kyony <ckyony@changamuka.com>
2014-05-03 10:26:39 +0200

    9.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     @current_user = user
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by( remember_token: remember_token )
@@ -26,7 +30,6 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
-
 
 
 
