b29907b36bb834fda30deb6eaeecf278cdaf930e
Christian Kyony <ckyony@changamuka.com>
2014-05-03 12:57:11 +0200

    9.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :signed_in_user, only: [:edit, :update, :index, :delete]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+  def destroy 
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   def index 
     @users = User.paginate(page: params[:page])
@@ -38,6 +45,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+   def admin_user 
+     redirect_to(root_url) unless current_user.admin?
+   end
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
