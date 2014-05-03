77abeb432f15d76f5c8bfb37c5e4ff1d84b4d309
Christian Kyony <ckyony@changamuka.com>
2014-05-03 16:34:59 +0200

    10.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,12 +56,6 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    def signed_in_user 
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in." 
-      end
-    end
 
     def correct_user 
       @user = User.find(params[:id])
